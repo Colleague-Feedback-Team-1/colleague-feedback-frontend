6ffbd8e09be10c11e48f4746ef914ad004b3bff7
Dang Le <haidang.levn@gmail.com>
2023-04-12 11:13:07 +0300

    Add route for requestSingle

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import UserDashboard from "./views/UserDashboard";
 import AdminDashboard from "./views/AdminDashboard";
 import Login from "./components/Login";
 import Layout from "./views/Layout";
+import RequestSingle from "./views/RequestSingle";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Login />}></Route>
           <Route path="/user" element={<UserDashboard />}></Route>
           <Route path="/admin" element={<AdminDashboard />}></Route>
+          <Route path="/requests/:requestId" element={<RequestSingle />}></Route>
         </Route>
       </Routes>
     </div>
