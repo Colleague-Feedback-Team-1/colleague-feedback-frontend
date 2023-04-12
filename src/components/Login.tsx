b998d6e3e12305f5fd5f8c511f01370e7b4bdf47
Dang Le <haidang.levn@gmail.com>
2023-04-12 11:14:53 +0300

    Update login page
    Login function is not ready

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,81 @@
-import { Button, Stack, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import {
+  Button,
+  Stack,
+  Typography,
+  TextField,
+  Box,
+  Avatar,
+  Grid,
+  FormControlLabel,
+  Checkbox, Link
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Login = () => {
   return (
     <>
-      <Typography variant="h3">Login to use the app</Typography>
-      <Stack direction={"row"} p={2} spacing={3} justifyContent={"center"}>
-        <Button variant="contained">
-          <Link to={"/admin"}>Admin</Link>
-        </Button>
-        <Button variant="contained">
-          <Link to={"/user"}>User</Link>
-        </Button>
-      </Stack>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in to use the app
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link  variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link  variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+ 
     </>
   );
 };
