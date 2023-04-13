4f995a9970cd4e9c8b4047360a81192bcc01c941
Dang Le <haidang.levn@gmail.com>
2023-04-13 10:27:16 +0300

    Update SearchBar function
    now can find all employees in the database

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,28 +1,59 @@
-import { Stack, InputBase, IconButton } from '@mui/material';
+import {
+  Stack,
+  InputBase,
+  IconButton,
+  Autocomplete,
+  TextField,
+  Box,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import { Employee } from "../types/types";
+import axios from "axios";
+import UserCard from "./UserCard";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-    return (
-      <Stack
-        direction={"row"}
-        style={{ backgroundColor: "white" }}
-        alignItems={"center"} 
-        justifyContent={'center'}
-        width={'350px'}
-      >
-        <InputBase
-          placeholder="Search…"
-          style={{
-            padding: "8px 12px",
-            width: "100%",
-          }}
-          inputProps={{ "aria-label": "search" }}
-        />
-        <IconButton aria-label="search" style={{ padding: 8 }}>
-          <SearchIcon />
-        </IconButton>
-      </Stack>
-    );
+  const [employees, setEmployees] = useState<Employee[] | null>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4500/api/employees/all-employees")
+      .then((res) => {
+        console.log(res.data);
+        setEmployees(res.data);
+      });
+  }, []);
+  return (
+    <Stack
+      direction={"row"}
+      style={{ backgroundColor: "white" }}
+      alignItems={"center"}
+      justifyContent={"center"}
+      width={"350px"}
+    >
+      <Autocomplete
+        options={employees!}
+        getOptionLabel={(employees) => `${employees.employeeName}`}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            placeholder="Search for an employee"
+          />
+        )}
+        sx={{ width: "100%" }}
+        noOptionsText="No employee found with that name"
+        renderOption={(props, employees) => (
+          <Link to={`/employees/${employees._id}`} {...employees}>
+            <Box component={"li"} {...props} key={employees._id}>
+              {employees.employeeName} ({employees.employeeEmail})
+            </Box>
+          </Link>
+        )}
+      />
+    </Stack>
+  );
 };
 
 export default SearchBar;
