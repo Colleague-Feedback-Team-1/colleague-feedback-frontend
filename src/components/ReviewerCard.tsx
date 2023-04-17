f22263793561a24a87f41b0e832e6b87a4ccc40b
Dang Le <haidang.levn@gmail.com>
2023-04-17 17:41:55 +0300

    Update Dashboard with request cards

--- src/components/ReviewerCard.tsx
+++ src/components/ReviewerCard.tsx
@@ -1,16 +1,23 @@
 import { Avatar, Card, CardHeader } from "@mui/material";
 import { Reviewer } from "../types/types";
+import { Link } from "react-router-dom";
 
-const ReviewerCard = (prop:Reviewer) => {
-    return (
-      <Card sx={{ width: "150px", height:'70px', margin:"10px" }}>
+const ReviewerCard = (prop: Reviewer) => {
+  return (
+    <Card sx={{ width: "250px", height: "70px", margin: "10px" }}>
+      <Link to={`/employees/${prop.reviewerid}`}>
         <CardHeader
-          avatar={<Avatar sx={{ bgcolor: "green" }}>{prop.reviewerName.slice(0,1)}</Avatar>}
-          title={prop.reviewerName}
-          subheader={`ID #${prop.reviewerEmail}`}
+          avatar={
+            <Avatar sx={{ bgcolor: "green" }}>
+              {prop.reviewerName.slice(0, 1)}
+            </Avatar>
+          }
+          title={`${prop.reviewerName} (${prop.role}) `}
+          subheader={`${prop.reviewerEmail} `}
         />
-      </Card>
-    );
+      </Link>
+    </Card>
+  );
 };
 
 export default ReviewerCard;
---
+++ src/components/UnconfirmedRequestCard.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import BlockIcon from "@mui/icons-material/Block";
+import { Stack } from "@mui/system";
+import { Request } from "../types/types";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const UnconfirmedRequestCard: React.FC<Request> = (prop) => {
+  return (
+    <Card sx={{ backgroundColor: "#ff6900", color: "white" }}>
+      <CardContent>
+        <Stack>
+          <Typography>Reviewee: {prop.employeeName}</Typography>
+          <Typography>Reviewer: {prop.reviewers.length}</Typography>
+          <Typography>Due date: {prop.dueDate}</Typography>
+        </Stack>
+      </CardContent>
+      <CardActions sx={{ width: "100%", bottom: 0, position: "relative" }}>
+        <Link
+          to={`/requests/${prop._id}`}
+          style={{ textDecoration: "none", paddingRight: "13px" }}
+        >
+          <Button variant="contained">View</Button>
+        </Link>
+
+        <Button variant="contained">Quick remind</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default UnconfirmedRequestCard;
