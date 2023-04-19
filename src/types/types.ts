71aa8f6ddc0f393e77d46e3ac9fe02dd0efd0cf3
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-19 12:28:53 +0300

    Changed Employee interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,39 +1,49 @@
+// export interface Employee {
+//   _id: string;
+//   employeeName: string;
+//   employeeEmail?: string;
+// //  hashedPassword?: string;
+//   companyRole?: string;
+//   image?: string;
+//   privileges?: "Admin" | "User" | "Manager";
+// }
 export interface Employee {
   _id: string;
-  employeeName: string;
-  employeeEmail?: string;
-//  hashedPassword?: string;
-  companyRole?: string;
-  image?: string;
-  privileges?: "Admin" | "User" | "Manager";
+  uid: string
+  displayName: string
+  givenName: string
+  uidNumber: number
+  gidNumber: number
+  mail: string
+  description: string
 }
 
 export interface UserContextProps {
-  user: Employee | null;
-  setUser: (user: Employee | null) => void;
+  user: Employee | null
+  setUser: (user: Employee | null) => void
 }
 
 export interface Request {
-  _id: string;
-  employeeid: string;
-  employeeName: string;
-  employeeEmail: string;
-  assignedManagerid: string | undefined;
-  assignedManagerName: string | undefined;
-  confirmedByHR: boolean;
-  selfReview: boolean;
-  dueDate?: string;
-  reviewers: Reviewer[];
-  createdAt?: string;
-  updatedAt?: string; 
+  _id: string
+  employeeid: string
+  employeeName: string
+  employeeEmail: string
+  assignedManagerid: string | undefined
+  assignedManagerName: string | undefined
+  confirmedByHR: boolean
+  selfReview: boolean
+  dueDate?: string
+  reviewers: Reviewer[]
+  createdAt?: string
+  updatedAt?: string
 }
 
 export interface Reviewer {
-  reviewerid: string;
-  reviewerName: string;
-  reviewerEmail: string;
-  role: string;
-  feedbackSubmitted: boolean;
+  reviewerid: string
+  reviewerName: string
+  reviewerEmail: string
+  role: string
+  feedbackSubmitted: boolean
 }
 
 type SectionNameOption =
@@ -43,22 +53,20 @@ type SectionNameOption =
   | 'Leadership'
   | 'Readiness for change'
   | 'Creativity'
-  | 'General Evaluation';
+  | 'General Evaluation'
 
 export interface FeedbackData {
-  requestId: string;
-  employeeId: string;
-  sections: Section[];
+  requestId: string
+  employeeId: string
+  sections: Section[]
 }
 
 export interface Section {
-  sectionName: SectionNameOption;
-  score: Score[];
+  sectionName: SectionNameOption
+  score: Score[]
 }
 
 export interface Score {
-  average: number;
-  openFeedback: string[];
+  average: number
+  openFeedback: string[]
 }
-
-
