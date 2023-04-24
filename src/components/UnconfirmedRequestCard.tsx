876af80f2dca0ff774523aa0df72ad9386435114
Dang Le <haidang.levn@gmail.com>
2023-04-24 17:49:14 +0300

    Add date converter

--- src/components/UnconfirmedRequestCard.tsx
+++ src/components/UnconfirmedRequestCard.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   Typography,
 } from "@mui/material";
 import { Stack } from "@mui/system";
@@ -11,13 +12,27 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 const UnconfirmedRequestCard: React.FC<Request> = (prop) => {
+  // convert the time from the time converter to the format that MongoDB accept
+  const timeFormatter = (date: any) => {
+    const formattedDate = date
+      .toLocaleDateString("en-US", {
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+      })
+      .replace(/\//g, ".");
+    return formattedDate;
+  };
   return (
     <Card sx={{ backgroundColor: "#ff6900", color: "white" }}>
+      <CardHeader
+        title={`ID ${prop._id.slice(0, 5)}...${prop._id.slice(-5)}`}
+      />
       <CardContent>
         <Stack>
           <Typography>Reviewee: {prop.employeeName}</Typography>
           <Typography>Reviewer: {prop.reviewers.length}</Typography>
-          <Typography>Due date: {prop.dueDate}</Typography>
+          <Typography>Due date: {prop.dateRequested}</Typography>
         </Stack>
       </CardContent>
       <CardActions sx={{ width: "100%", bottom: 0, position: "relative" }}>
@@ -28,7 +43,6 @@ const UnconfirmedRequestCard: React.FC<Request> = (prop) => {
           <Button variant="contained">View</Button>
         </Link>
 
-        
         <Button variant="contained">Quick remind</Button>
       </CardActions>
     </Card>
