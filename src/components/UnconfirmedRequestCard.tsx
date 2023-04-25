2b355af43e3cf0f714ceb5615f45cd338789bf63
Dang Le <haidang.levn@gmail.com>
2023-04-25 10:39:31 +0300

    Add time converter

--- src/components/UnconfirmedRequestCard.tsx
+++ src/components/UnconfirmedRequestCard.tsx
@@ -9,20 +9,17 @@ import {
 import { Stack } from "@mui/system";
 import { Request } from "../types/types";
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 
 const UnconfirmedRequestCard: React.FC<Request> = (prop) => {
   // convert the time from the time converter to the format that MongoDB accept
-  const timeFormatter = (date: any) => {
-    const formattedDate = date
-      .toLocaleDateString("en-US", {
-        month: "2-digit",
-        day: "2-digit",
-        year: "numeric",
-      })
-      .replace(/\//g, ".");
-    return formattedDate;
+  const formatDate = (date: string) => {
+    let outputDate = new Date(date).toLocaleDateString();
+    return outputDate;
   };
+
+ 
+  
   return (
     <Card sx={{ backgroundColor: "#ff6900", color: "white" }}>
       <CardHeader
@@ -32,7 +29,7 @@ const UnconfirmedRequestCard: React.FC<Request> = (prop) => {
         <Stack>
           <Typography>Reviewee: {prop.employeeName}</Typography>
           <Typography>Reviewer: {prop.reviewers.length}</Typography>
-          <Typography>Due date: {prop.dateRequested}</Typography>
+          <Typography>Due date: {formatDate(prop.dateRequested!)}</Typography>
         </Stack>
       </CardContent>
       <CardActions sx={{ width: "100%", bottom: 0, position: "relative" }}>
