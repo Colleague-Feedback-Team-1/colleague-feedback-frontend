c407932d6ef78de4926b7269c51447d11072ba61
Dang Le <haidang.levn@gmail.com>
2023-04-25 10:40:05 +0300

    Update UserDashboard.tsx

--- src/views/UserDashboard.tsx
+++ src/views/UserDashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, IconButton } from "@mui/material";
 import { Stack } from "@mui/system";
 import { Request } from "../types/types";
 import { useEffect, useState, useContext } from "react";
@@ -9,14 +9,19 @@ import UserContext from "../context/UserContext";
 import { UserContextProps } from "../types/types";
 import UnconfirmedRequestCard from "../components/UnconfirmedRequestCard";
 import { Link } from "react-router-dom";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import SplitscreenOutlinedIcon from "@mui/icons-material/SplitscreenOutlined";
+import ViewListOutlinedIcon from "@mui/icons-material/ViewListOutlined";
 
 const UserDashboard = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [requestList, setRequestList] = useState<Request[] | null>();
+  const [asReviewerList, setAsReviewerList] = useState<Request[] | null>();
   const [unconfirmedRequest, setUnconfirmedRequest] = useState<
     Request[] | null
   >();
   const { user } = useContext<UserContextProps>(UserContext);
+  const [view, setView] = useState<"row" | "grid">("row");
 
   // Date display
   const date = new Date().toLocaleString("en-US", {
@@ -43,6 +48,14 @@ const UserDashboard = () => {
           setRequestList(res.data);
         })
         .catch((err) => console.log(err));
+      axios
+        .get(
+          `http://localhost:4500/api/review-requests/as-reviewer/${user!._id}`
+        )
+        .then((res) => {
+          setAsReviewerList(res.data);
+        })
+        .catch((err) => console.log(err));
       setIsLoading(false);
 
       if (user?.description === "HR") {
@@ -59,17 +72,85 @@ const UserDashboard = () => {
         <Loading />
       ) : (
         <div>
-          <Box paddingBottom={"50px"}>
-            <Typography variant="h3">Hello, {user!.displayName}</Typography>
-            <Typography variant="h6">Today is {date}</Typography>
-          </Box>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            paddingBottom={"50px"}
+          >
+            <Box>
+              <Typography variant="h3">Hello, {user!.displayName}</Typography>
+              <Typography variant="h6">Today is {date}</Typography>
+            </Box>
+            <Link to={"/requests/createNewRequest"}>
+              <Button variant="contained" size="large" color="success">
+                Create New Request
+              </Button>
+            </Link>
+          </Stack>
+
+          <Stack
+            direction={"row"}
+            spacing={1}
+            alignItems={"center"}
+            justifyContent={"flex-end"}
+            marginBottom={"30px"}
+          >
+            <Typography>View</Typography>
+            {view === "row" ? (
+              <>
+                <IconButton
+                  sx={{
+                    border: "0.5px solid black",
+                    borderRadius: 2,
+                    backgroundColor: "blue",
+                    color: "white",
+                  }}
+                >
+                  <SplitscreenOutlinedIcon />
+                </IconButton>
+                <IconButton
+                  sx={{ border: "0.5px solid black", borderRadius: 2 }}
+                  onClick={() => setView("grid")}
+                >
+                  <ViewListOutlinedIcon />
+                </IconButton>
+              </>
+            ) : (
+              <>
+                <IconButton
+                  sx={{ border: "0.5px solid black", borderRadius: 2 }}
+                  onClick={() => setView("row")}
+                >
+                  <SplitscreenOutlinedIcon />
+                </IconButton>
+                <IconButton
+                  color="primary"
+                  sx={{
+                    border: "0.5px solid black",
+                    borderRadius: 2,
+                    backgroundColor: "blue",
+                    color: "white",
+                  }}
+                >
+                  <ViewListOutlinedIcon />
+                </IconButton>
+              </>
+            )}
+          </Stack>
 
           {user?.description === "HR" ? (
             <Box paddingBottom={"50px"}>
               <Typography variant="h4">
                 [Admin] Unconfirmed requests:
               </Typography>
-              <Stack direction={"row"} spacing={2}>
+              <Stack
+                direction={"row"}
+                spacing={"2px"}
+                flexWrap={"wrap"}
+                gap={"20px"}
+                paddingY={"10px"}
+              >
                 {unconfirmedRequest ? (
                   unconfirmedRequest!.map((request) => {
                     return (
@@ -80,6 +161,9 @@ const UserDashboard = () => {
                   <p>You have no requests</p>
                 )}
               </Stack>
+              <Button variant="outlined" color="info">
+                See all
+              </Button>
             </Box>
           ) : (
             <></>
@@ -87,7 +171,13 @@ const UserDashboard = () => {
 
           <Box paddingBottom={"50px"}>
             <Typography variant="h4">Your feedback requests:</Typography>
-            <Stack direction={"row"} spacing={2}>
+            <Stack
+              direction={"row"}
+              spacing={"2px"}
+              flexWrap={"wrap"}
+              gap={"20px"}
+              paddingY={"10px"}
+            >
               {requestList ? (
                 requestList!.map((request) => {
                   return <RequestCard {...request} key={request._id} />;
@@ -96,15 +186,35 @@ const UserDashboard = () => {
                 <p>You have no requests</p>
               )}
             </Stack>
-          </Box>
-          <Typography variant="h4">
-            Your co-worker needs your feedback:{" "}
-          </Typography>
-          <Link to={"/requests/createNewRequest"}>
-            <Button variant="contained" size="large">
-              Create New Request
+            <Button variant="outlined" color="info">
+              See all
             </Button>
-          </Link>
+          </Box>
+          <Box paddingBottom={"50px"}>
+            <Typography variant="h4">
+              Your co-worker needs your feedback:{" "}
+            </Typography>
+            <Stack
+              direction={"row"}
+              spacing={"2px"}
+              flexWrap={"wrap"}
+              gap={"20px"}
+              paddingY={"10px"}
+            >
+              {asReviewerList ? (
+                asReviewerList!.map((request) => {
+                  return <RequestCard {...request} key={request._id} />;
+                })
+              ) : (
+                <Typography>
+                  You have no review requests from your colleagues
+                </Typography>
+              )}
+            </Stack>
+            <Button variant="outlined" color="info">
+              See all
+            </Button>
+          </Box>
         </div>
       )}
     </Stack>
