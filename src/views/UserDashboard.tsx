ec8fe20b3565e8adb29050650bf18913478f2da1
Dang Le <haidang.levn@gmail.com>
2023-04-26 18:09:03 +0300

    Render dashboard by role

--- src/views/UserDashboard.tsx
+++ src/views/UserDashboard.tsx
@@ -10,31 +10,26 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Stack,
 } from "@mui/material";
-import { Stack } from "@mui/system";
 import { Request } from "../types/types";
 import { useEffect, useState, useContext } from "react";
 import Loading from "../components/Loading";
 import RequestCard from "../components/RequestCard";
 import axios from "axios";
 import UserContext from "../context/UserContext";
-import { UserContextProps } from "../types/types";
-import UnconfirmedRequestCard from "../components/UnconfirmedRequestCard";
+import { UserContextProps, Reviewer } from "../types/types";
 import { Link } from "react-router-dom";
-import SplitscreenOutlinedIcon from "@mui/icons-material/SplitscreenOutlined";
-import ViewListOutlinedIcon from "@mui/icons-material/ViewListOutlined";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import type {} from "@mui/x-data-grid/themeAugmentation";
+import RequestDashboard from "../components/RequestDataGrid";
+
 const UserDashboard = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [requestList, setRequestList] = useState<Request[] | null>();
   const [adminRequestList, setAdminRequestList] = useState<Request[] | null>();
   const [asReviewerList, setAsReviewerList] = useState<Request[] | null>();
-  const [unconfirmedRequest, setUnconfirmedRequest] = useState<
-    Request[] | null
-  >();
+ 
   const { user } = useContext<UserContextProps>(UserContext);
-  const [view, setView] = useState<"basic" | "table">("table");
 
   // Date display
   const date = new Date().toLocaleString("en-US", {
@@ -44,23 +39,14 @@ const UserDashboard = () => {
   });
 
   useEffect(() => {
-    const fetchingAdminData = async () => {
-      // Admin can fetch unconfirmed requests
-      axios
-        .get(`http://localhost:4500/api/review-requests/to-confirm`)
-        .then((res) => {
-          console.log(res.data);
-          setUnconfirmedRequest(res.data);
-        });
-
+    const fetchAdminData = async () => {
       // Admin can fetch all requests in db
       axios.get(`http://localhost:4500/api/review-requests/`).then((res) => {
         setAdminRequestList(res.data);
       });
     };
 
-    // Create a quick loading duration
-    setTimeout(() => {
+    const fetchUserData = async () => {
       axios
         .get(
           `http://localhost:4500/api/review-requests/by-employeeid/${user!._id}`
@@ -77,49 +63,24 @@ const UserDashboard = () => {
           setAsReviewerList(res.data);
         })
         .catch((err) => console.log(err));
-      setIsLoading(false);
+    };
 
+    // Create a quick loading duration
+    setTimeout(() => {
       if (user?.description === "HR") {
-        fetchingAdminData();
+        fetchAdminData();
+      } else {
+        fetchUserData();
       }
+      setIsLoading(false);
     }, 1500);
   }, [user]);
 
   // render the page depends on the view choosen
   const renderDashboard = () => {
-    if (view === "basic") {
+    if (user?.description !== "HR") {
       return (
         <>
-          {user?.description === "HR" ? (
-            <Box paddingBottom={"50px"}>
-              <Typography variant="h4">
-                [Admin] Unconfirmed requests:
-              </Typography>
-              <Stack
-                direction={"row"}
-                spacing={"2px"}
-                flexWrap={"wrap"}
-                gap={"20px"}
-                paddingY={"10px"}
-              >
-                {unconfirmedRequest ? (
-                  unconfirmedRequest!.map((request) => {
-                    return (
-                      <UnconfirmedRequestCard {...request} key={request._id} />
-                    );
-                  })
-                ) : (
-                  <p>You have no requests</p>
-                )}
-              </Stack>
-              <Button variant="outlined" color="info">
-                See all
-              </Button>
-            </Box>
-          ) : (
-            <></>
-          )}
-
           <Box paddingBottom={"50px"}>
             <Typography variant="h4">Your feedback requests:</Typography>
             <Stack
@@ -134,12 +95,10 @@ const UserDashboard = () => {
                   return <RequestCard {...request} key={request._id} />;
                 })
               ) : (
-                <p>You have no requests</p>
+                <Typography>You have no requests</Typography>
               )}
             </Stack>
-            <Button variant="outlined" color="info">
-              See all
-            </Button>
+            
           </Box>
 
           <Box paddingBottom={"50px"}>
@@ -163,61 +122,12 @@ const UserDashboard = () => {
                 </Typography>
               )}
             </Stack>
-            <Button variant="outlined" color="info">
-              See all
-            </Button>
+            
           </Box>
         </>
       );
-    } else if (view === "table") {
-
-     /*  const columns = [
-        { field: "_id", headerName: "ID", width: 200 },
-        { field: "employeeName", headerName: "Employee Name", width: 200 },
-        { field: "selfReview", headerName: "Self Review", width: 150 },
-        { field: "confirmedByHR", headerName: "Confirmed By HR", width: 200 },
-        {
-          field: "feedbackReceived",
-          headerName: "Feedback Received",
-          width: 200,
-          valueGetter: (params:any) => {
-            const feedbackSubmitted = params.row.reviewers.filter(
-              (reviewer:any) => reviewer.feedbackSubmitted
-            );
-            return `${feedbackSubmitted.length}/${params.row.reviewers.length}`;
-          },
-        },
-      ]; */
-      return (
-        <>
-          <TableContainer component={Paper}>
-            <Table sx={{ marginBottom: "50px" }}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>RequestID</TableCell>
-                  <TableCell>Confirmed</TableCell>
-                  <TableCell>Reviewee</TableCell>
-                  <TableCell>Self Review</TableCell>
-                  <TableCell>Reviewers</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {adminRequestList?.map((request) => (
-                  <TableRow key={request._id}>
-                    <TableCell>{`...${request._id.slice(-6)}`}</TableCell>
-                    <TableCell>{request.confirmedByHR.toString()}</TableCell>
-                    <TableCell>{request.employeeName}</TableCell>
-                    <TableCell>{request.selfReview.toString()}</TableCell>
-                    <TableCell>{request.reviewers.length}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          {/*           <DataGrid rows={adminRequestList!} columns={columns} />
-           */}{" "}
-        </>
-      );
+    } else {
+      return <RequestDashboard />;
     }
   };
 
@@ -244,55 +154,6 @@ const UserDashboard = () => {
             </Link>
           </Stack>
 
-          <Stack
-            direction={"row"}
-            spacing={1}
-            alignItems={"center"}
-            justifyContent={"flex-end"}
-            marginBottom={"30px"}
-          >
-            <Typography>View</Typography>
-            {view === "basic" ? (
-              <>
-                <IconButton
-                  sx={{
-                    border: "0.5px solid black",
-                    borderRadius: 2,
-                    backgroundColor: "blue",
-                    color: "white",
-                  }}
-                >
-                  <SplitscreenOutlinedIcon />
-                </IconButton>
-                <IconButton
-                  sx={{ border: "0.5px solid black", borderRadius: 2 }}
-                  onClick={() => setView("table")}
-                >
-                  <ViewListOutlinedIcon />
-                </IconButton>
-              </>
-            ) : (
-              <>
-                <IconButton
-                  sx={{ border: "0.5px solid black", borderRadius: 2 }}
-                  onClick={() => setView("basic")}
-                >
-                  <SplitscreenOutlinedIcon />
-                </IconButton>
-                <IconButton
-                  color="primary"
-                  sx={{
-                    border: "0.5px solid black",
-                    borderRadius: 2,
-                    backgroundColor: "blue",
-                    color: "white",
-                  }}
-                >
-                  <ViewListOutlinedIcon />
-                </IconButton>
-              </>
-            )}
-          </Stack>
           <Box>{renderDashboard()}</Box>
         </div>
       )}
@@ -301,8 +162,3 @@ const UserDashboard = () => {
 };
 
 export default UserDashboard;
-
-/* 
-// Use basic table from MUI
-        
-          /*  */
