0ae5f428e0d32086191b932198f350da858a8087
Dang Le <haidang.levn@gmail.com>
2023-04-27 15:12:43 +0300

    Add userRoleOnRequest check
    To show the right card action/ limit access to the form

--- src/views/RequestSingle.tsx
+++ src/views/RequestSingle.tsx
@@ -1,4 +1,11 @@
-import { Box, Typography, Stack, Button, Card } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Stack,
+  Button,
+  Card,
+  LinearProgress,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -19,7 +26,9 @@ const RequestSingle = () => {
     "reviewee" | "reviewer" | "manager" | null
   >(null);
 
-  console.log(requestData);
+  const feedbackSubmitted = requestData?.reviewers.filter(
+    (reviewer: any) => reviewer.feedbackSubmitted
+  );
 
   const formatDate = (date: string) => {
     let outputDate = new Date(date).toLocaleString();
@@ -53,13 +62,41 @@ const RequestSingle = () => {
       setUserRoleOnRequest("manager");
     } else {
       requestData?.reviewers.map((reviewer) => {
-        if (user?._id === reviewer.reviewerid && reviewer.feedbackSubmitted === false) {
+        if (
+          user?._id === reviewer.reviewerid &&
+          reviewer.feedbackSubmitted === false
+        ) {
           console.log(
             "You are one the reviewer of this request! You can give review."
           );
           setUserRoleOnRequest("reviewer");
         }
       });
+    }
+  };
+
+  // render feedback received slider
+  const renderSlider = () => {
+    const value =
+      (feedbackSubmitted?.length! / requestData?.reviewers.length!) * 100;
+    console.log("Slider value: ", value);
+    if (value < 80) {
+      return (
+        <LinearProgress
+          variant="determinate"
+          value={value}
+          sx={{ width: "70%", height: "20px", borderRadius: '10px' }}
+          color="error"
+        />
+      );
+    } else {
+      return (
+        <LinearProgress
+          variant="determinate"
+          value={value}
+          sx={{ width: "70%", height: "20px", borderRadius: '10px' }}
+          color="success"
+        />)
     }
   };
 
@@ -121,30 +158,38 @@ const RequestSingle = () => {
         );
       }
     } else {
-      if (userRoleOnRequest === 'reviewee' && requestData?.selfReview === false) {
+      if (
+        userRoleOnRequest === "reviewee" &&
+        requestData?.selfReview === false
+      ) {
         return (
-          <Stack direction={'row'} spacing={2}>
+          <>
+            <Stack direction={"row"} spacing={2}>
+              <Link to={"/dashboard"}>
+                <Button variant="contained">Back to dashboard</Button>
+              </Link>
+              <Link to={`/submission-form/${params.requestId}`}>
+                <Button variant="contained" color="success">
+                  Self Review
+                </Button>
+              </Link>
+            </Stack>
+          </>
+        );
+      } else if (userRoleOnRequest !== null) {
+        return (
+          <Stack direction={"row"} spacing={2}>
             <Link to={"/dashboard"}>
               <Button variant="contained">Back to dashboard</Button>
             </Link>
             <Link to={`/submission-form/${params.requestId}`}>
-              <Button variant="contained" color="success">Self Review</Button>
+              <Button variant="contained" color="success">
+                Give feedback (as {userRoleOnRequest})
+              </Button>
             </Link>
           </Stack>
         );
-      }
-      else if (userRoleOnRequest !== null) {
-        return (
-          <Stack direction={'row'} spacing={2}>
-            <Link to={"/dashboard"}>
-              <Button variant="contained">Back to dashboard</Button>
-            </Link>
-            <Link to={`/submission-form/${params.requestId}`}>
-              <Button variant="contained" color="success">Give feedback (as {userRoleOnRequest})</Button>
-            </Link>
-          </Stack>)
-      }
-      else {
+      } else {
         return (
           <Stack direction={"row"} spacing={2}>
             <Link to={"/dashboard"}>
@@ -160,79 +205,92 @@ const RequestSingle = () => {
     <Stack sx={{ textAlign: "left", paddingBottom: "30px" }}>
       {isLoading === false ? (
         <Card sx={{ padding: "20px", backgroundColor: "#ffdbeb" }}>
-          <Box paddingBottom={"50px"} component={"div"}>
-            <Typography variant="h3">
-              REQUEST #
-              {`${requestData?._id.slice(0, 5)}...${requestData?._id.slice(
-                -3
-              )}`}
-            </Typography>
+          <Stack direction={"row"} spacing={10}>
+            <Box paddingBottom={"50px"} component={"div"}>
+              <Typography variant="h3">
+                REQUEST ID #{`...${requestData?._id.slice(-7)}`}
+              </Typography>
 
-            {requestData?.confirmedByHR ? (
-              <Stack
-                direction={"row"}
-                spacing={1}
-                sx={{ alignItems: "center", pb: 4 }}
-              >
-                <CheckCircleIcon color="success" />
-                <Typography variant="body2">
-                  This request has been confirmed, reviewers can start giving
-                  feedback now.
-                </Typography>
+              {requestData?.confirmedByHR ? (
+                <Stack
+                  direction={"row"}
+                  spacing={1}
+                  sx={{ alignItems: "center", pb: 4 }}
+                >
+                  <CheckCircleIcon color="success" />
+                  <Typography variant="body2">
+                    This request has been confirmed, reviewers can start giving
+                    feedback now.
+                  </Typography>
+                </Stack>
+              ) : (
+                <></>
+              )}
+
+              <Typography variant="body1">
+                <b>Created At: </b>
+                {formatDate(requestData?.createdAt!)}
+              </Typography>
+              <Typography variant="body1">
+                <b>Due date: </b>
+                {formatDate(requestData?.dateRequested!)}
+              </Typography>
+              <Typography variant="body1">
+                <b>Status: </b>
+                {requestData!.confirmedByHR ? (
+                  <span style={{ color: "green" }}>Confirmed by HR</span>
+                ) : (
+                  <span style={{ color: "red" }}>Not confirmed</span>
+                )}
+              </Typography>
+              <Typography variant="body1">
+                <b>Self review: </b>
+                {requestData!.selfReview ? (
+                  <span style={{ color: "green" }}>Yes</span>
+                ) : (
+                  <span style={{ color: "red" }}>No</span>
+                )}
+              </Typography>
+              <Typography>
+                <b>
+                  {" "}
+                  Feedbacks received:{" "}
+                  {`${feedbackSubmitted?.length}/${requestData?.reviewers.length}`}
+                </b>
+              </Typography>
+              {renderSlider()}
+            </Box>
+
+            <Box paddingBottom={"50px"} component={"div"}>
+              <Stack direction={"row"}>
+                <Box>
+                  <Typography variant="h4">Reviewee:</Typography>
+                  <EmployeeCard {...requestData!} />
+                </Box>
+                <Box component={"div"}>
+                  <Typography variant="h4">Project Manager:</Typography>
+                  {managerData ? (
+                    <EmployeeCard
+                      employeeid={managerData?._id}
+                      employeeEmail={managerData!.mail}
+                      employeeName={managerData!.displayName}
+                      selfReview={null}
+                    />
+                  ) : (
+                    <Typography>No manager assigned yet.</Typography>
+                  )}
+                </Box>
               </Stack>
-            ) : (
-              <></>
-            )}
 
-            <Typography variant="body1">
-              <b>Created At: </b>
-              {formatDate(requestData?.createdAt!)}
-            </Typography>
-            <Typography variant="body1">
-              <b>Due date: </b>
-              {formatDate(requestData?.dateRequested!)}
-            </Typography>
-            <Typography variant="body1">
-              <b>Status: </b>
-              {requestData!.confirmedByHR ? (
-                <span style={{ color: "green" }}>Confirmed by HR</span>
-              ) : (
-                <span style={{ color: "red" }}>Not confirmed</span>
-              )}
-            </Typography>
-            <Typography variant="body1">
-              <b>Self review: </b>
-              {requestData!.selfReview ? (
-                <span style={{ color: "green" }}>Yes</span>
-              ) : (
-                <span style={{ color: "red" }}>No</span>
-              )}
-            </Typography>
-          </Box>
-          <Box paddingBottom={"50px"} component={"div"}>
-            <Typography variant="h4">Project Manager:</Typography>
-            {managerData ? (
-              <EmployeeCard
-                employeeid={managerData?._id}
-                employeeEmail={managerData!.mail}
-                employeeName={managerData!.displayName}
-              />
-            ) : (
-              <Typography>No manager assigned yet.</Typography>
-            )}
-          </Box>
+              <Typography variant="h4">Reviewers:</Typography>
+              <Stack direction={"row"} flexWrap={"wrap"}>
+                {requestData!.reviewers.map((reviewer) => {
+                  return <ReviewerCard {...reviewer} />;
+                })}
+              </Stack>
+            </Box>
+          </Stack>
 
-          <Box paddingBottom={"50px"} component={"div"}>
-            <Typography variant="h4">Reviewee:</Typography>
-            <EmployeeCard {...requestData!} />
-            <Typography variant="h4">Reviewers:</Typography>
-
-            <Stack direction={"row"} flexWrap={"wrap"}>
-              {requestData!.reviewers.map((reviewer) => {
-                return <ReviewerCard {...reviewer} />;
-              })}
-            </Stack>
-          </Box>
           {renderCardAction()}
         </Card>
       ) : (
