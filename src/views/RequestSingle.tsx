e0a183274cb9b37e532adbab173c16f6e0a3e7d5
Dang Le <haidang.levn@gmail.com>
2023-04-27 12:18:59 +0300

    Add check role on request logic
    and some prettier

--- src/views/RequestSingle.tsx
+++ src/views/RequestSingle.tsx
@@ -1,124 +1,183 @@
-import { Box, Typography, Stack, Button, Card } from '@mui/material'
-import axios from 'axios'
-import { useEffect, useState, useContext } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import EmployeeCard from '../components/EmployeeCard'
-import ReviewerCard from '../components/ReviewerCard'
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import { Employee, Request, UserContextProps } from '../types/types'
-import Loading from '../components/Loading'
-import UserContext from '../context/UserContext'
+import { Box, Typography, Stack, Button, Card } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import EmployeeCard from "../components/EmployeeCard";
+import ReviewerCard from "../components/ReviewerCard";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Employee, Request, UserContextProps } from "../types/types";
+import Loading from "../components/Loading";
+import UserContext from "../context/UserContext";
 
 const RequestSingle = () => {
-  const params = useParams()
-  const [requestData, setRequestData] = useState<Request | null>()
-  const [isLoading, setIsLoading] = useState(true)
-  const [managerData, setManagerData] = useState<Employee | null>()
-  const { user } = useContext<UserContextProps>(UserContext)
+  const params = useParams();
+  const [requestData, setRequestData] = useState<Request | null>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [managerData, setManagerData] = useState<Employee | null>();
+  const { user } = useContext<UserContextProps>(UserContext);
+  const [userRoleOnRequest, setUserRoleOnRequest] = useState<
+    "reviewee" | "reviewer" | "manager" | null
+  >(null);
+
+  console.log(requestData);
 
   const formatDate = (date: string) => {
-    let outputDate = new Date(date).toLocaleString()
-    return outputDate
-  }
+    let outputDate = new Date(date).toLocaleString();
+    return outputDate;
+  };
 
   // fetch data for the manager from requestData.assignedManagerid
   const fetchManager = () => {
     if (requestData?.assignedManagerid) {
-      console.log('This request has managerId', requestData.assignedManagerid)
       axios
-        .get(`http://localhost:4500/api/employees/${requestData!.assignedManagerid}`)
+        .get(
+          `http://localhost:4500/api/employees/${
+            requestData!.assignedManagerid
+          }`
+        )
         .then((res) => {
-          console.log(res.data)
-          setManagerData(res.data)
-        })
+          setManagerData(res.data);
+        });
     }
-  }
+  };
+
+  // check role
+  const checkRole = () => {
+    if (user?._id == requestData?.employeeid) {
+      console.log("You are the reviewee of this request! You can self review.");
+      setUserRoleOnRequest("reviewee");
+    } else if (user?._id == requestData?.assignedManagerid) {
+      console.log(
+        "You are the manager of this request! You can give review and see the result"
+      );
+      setUserRoleOnRequest("manager");
+    } else {
+      requestData?.reviewers.map((reviewer) => {
+        if (user?._id === reviewer.reviewerid && reviewer.feedbackSubmitted === false) {
+          console.log(
+            "You are one the reviewer of this request! You can give review."
+          );
+          setUserRoleOnRequest("reviewer");
+        }
+      });
+    }
+  };
 
   useEffect(() => {
-    console.log('requestData changed, check for managerId')
-    if (requestData?.assignedManagerid) {
-      console.log(requestData.assignedManagerid)
-      fetchManager()
-    } else {
-      console.log('no managerid yet')
-    }
-  }, [requestData])
+    // run check role after data is fetched
+    fetchManager();
+    checkRole();
+  }, [requestData]);
+
   /* Fetch data of the request */
   useEffect(() => {
     setTimeout(() => {
       axios
-        .get(`http://localhost:4500/api/review-requests/by-requestid/${params.requestId}`)
+        .get(
+          `http://localhost:4500/api/review-requests/by-requestid/${params.requestId}`
+        )
         .then((res) => {
-          setRequestData(res.data)
+          setRequestData(res.data);
           if (requestData?.assignedManagerid) {
-            fetchManager()
+            fetchManager();
           }
-          setIsLoading(false)
-        })
-    }, 2000)
-  }, [params.requestId])
+          setIsLoading(false);
+        });
+    }, 2000);
+  }, [params.requestId]);
 
   // check the confirm status and user priviledge to render the action buttons
   const renderCardAction = () => {
     if (requestData?.confirmedByHR === false) {
-      if (user?.description === 'HR') {
+      if (user?.description === "HR") {
         return (
           <>
             <Typography variant="body1">
-              HR can click the "Confirm this request" to assign an manager and then confirm this
-              request.
+              HR can click the "Confirm this request" to assign an manager and
+              then confirm this request.
             </Typography>
-            <Stack direction={'row'} justifyContent={'space-between'}>
-              <Stack spacing={2} direction={'row'}>
+            <Stack direction={"row"} justifyContent={"space-between"}>
+              <Stack spacing={2} direction={"row"}>
                 <Button variant="contained" color="error">
                   Reject this request
                 </Button>
                 <Link to={`/requests/${params.requestId}/confirm`}>
-                  <Button variant="contained" color="success" /* onClick={handleConfirm} */>
+                  <Button
+                    variant="contained"
+                    color="success" /* onClick={handleConfirm} */
+                  >
                     Confirm this request
                   </Button>
                 </Link>
               </Stack>
 
-              <Link to={'/dashboard'}>
+              <Link to={"/dashboard"}>
                 <Button variant="contained" color="info">
                   Back to dashboard
                 </Button>
               </Link>
             </Stack>
           </>
-        )
+        );
       }
     } else {
-      return (
-        <>
-          <Link to={'/dashboard'}>
-            <Button variant="outlined">Back to dashboard</Button>
-          </Link>
-          <Link to={`/submission-form/${params.requestId}`}>
-            <Button variant="outlined">Fill Feedback Form</Button>
-          </Link>
-        </>
-      )
+      if (userRoleOnRequest === 'reviewee' && requestData?.selfReview === false) {
+        return (
+          <Stack direction={'row'} spacing={2}>
+            <Link to={"/dashboard"}>
+              <Button variant="contained">Back to dashboard</Button>
+            </Link>
+            <Link to={`/submission-form/${params.requestId}`}>
+              <Button variant="contained" color="success">Self Review</Button>
+            </Link>
+          </Stack>
+        );
+      }
+      else if (userRoleOnRequest !== null) {
+        return (
+          <Stack direction={'row'} spacing={2}>
+            <Link to={"/dashboard"}>
+              <Button variant="contained">Back to dashboard</Button>
+            </Link>
+            <Link to={`/submission-form/${params.requestId}`}>
+              <Button variant="contained" color="success">Give feedback (as {userRoleOnRequest})</Button>
+            </Link>
+          </Stack>)
+      }
+      else {
+        return (
+          <Stack direction={"row"} spacing={2}>
+            <Link to={"/dashboard"}>
+              <Button variant="contained">Back to dashboard</Button>
+            </Link>
+          </Stack>
+        );
+      }
     }
-  }
+  };
 
   return (
-
-    <Stack sx={{ textAlign: "left", paddingBottom: '30px' }}>
+    <Stack sx={{ textAlign: "left", paddingBottom: "30px" }}>
       {isLoading === false ? (
         <Card sx={{ padding: "20px", backgroundColor: "#ffdbeb" }}>
           <Box paddingBottom={"50px"} component={"div"}>
-
             <Typography variant="h3">
-              REQUEST #{`${requestData?._id.slice(0, 5)}...${requestData?._id.slice(-3)}`}
+              REQUEST #
+              {`${requestData?._id.slice(0, 5)}...${requestData?._id.slice(
+                -3
+              )}`}
             </Typography>
 
             {requestData?.confirmedByHR ? (
-              <Stack direction={'row'} spacing={1} sx={{ alignItems: 'center', pb: 4 }}>
+              <Stack
+                direction={"row"}
+                spacing={1}
+                sx={{ alignItems: "center", pb: 4 }}
+              >
                 <CheckCircleIcon color="success" />
                 <Typography variant="body2">
-                  This request has been confirmed, reviewers can start giving feedback now.
+                  This request has been confirmed, reviewers can start giving
+                  feedback now.
                 </Typography>
               </Stack>
             ) : (
@@ -136,13 +195,21 @@ const RequestSingle = () => {
             <Typography variant="body1">
               <b>Status: </b>
               {requestData!.confirmedByHR ? (
-                <span style={{ color: 'green' }}>Confirmed by HR</span>
+                <span style={{ color: "green" }}>Confirmed by HR</span>
               ) : (
-                <span style={{ color: 'red' }}>Not confirmed</span>
+                <span style={{ color: "red" }}>Not confirmed</span>
+              )}
+            </Typography>
+            <Typography variant="body1">
+              <b>Self review: </b>
+              {requestData!.selfReview ? (
+                <span style={{ color: "green" }}>Yes</span>
+              ) : (
+                <span style={{ color: "red" }}>No</span>
               )}
             </Typography>
           </Box>
-          <Box paddingBottom={'50px'} component={'div'}>
+          <Box paddingBottom={"50px"} component={"div"}>
             <Typography variant="h4">Project Manager:</Typography>
             {managerData ? (
               <EmployeeCard
@@ -155,14 +222,14 @@ const RequestSingle = () => {
             )}
           </Box>
 
-          <Box paddingBottom={'50px'} component={'div'}>
+          <Box paddingBottom={"50px"} component={"div"}>
             <Typography variant="h4">Reviewee:</Typography>
             <EmployeeCard {...requestData!} />
             <Typography variant="h4">Reviewers:</Typography>
 
             <Stack direction={"row"} flexWrap={"wrap"}>
- {requestData!.reviewers.map((reviewer) => {
-                return <ReviewerCard {...reviewer} />
+              {requestData!.reviewers.map((reviewer) => {
+                return <ReviewerCard {...reviewer} />;
               })}
             </Stack>
           </Box>
@@ -172,7 +239,7 @@ const RequestSingle = () => {
         <Loading />
       )}
     </Stack>
-  )
-}
+  );
+};
 
-export default RequestSingle
+export default RequestSingle;
