ec3e8f1df844e36539668470cbddde016bd38737
silvercoyote97 <haidang.levn@gmail.com>
2023-05-02 08:51:30 +0300

    Update enum type for Employee interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,62 +1,62 @@
 export interface Employee {
-  _id: string
-  uid: string
-  displayName: string
-  givenName: string
-  uidNumber: number
-  gidNumber: number
-  mail: string
-  description: string
+  _id: string;
+  uid: string;
+  displayName: string;
+  givenName: string;
+  uidNumber: number;
+  gidNumber: number;
+  mail: string;
+  description: "HR" | "Manager" | "user";
 }
 
 export interface UserContextProps {
-  user: Employee | null
-  setUser: (user: Employee | null) => void
+  user: Employee | null;
+  setUser: (user: Employee | null) => void;
 }
 
 export interface Request {
-  _id: string
-  employeeid: string
-  employeeName: string
-  employeeEmail: string
-  assignedManagerid: string | undefined
-  assignedManagerName: string | undefined
-  confirmedByHR: boolean
-  selfReview: boolean
-  dateRequested?: string
-  reviewers: Reviewer[]
-  createdAt?: string
-  updatedAt?: string
+  _id: string;
+  employeeid: string;
+  employeeName: string;
+  employeeEmail: string;
+  assignedManagerid: string | undefined;
+  assignedManagerName: string | undefined;
+  confirmedByHR: boolean;
+  selfReview: boolean;
+  dateRequested?: string;
+  reviewers: Reviewer[];
+  createdAt?: string;
+  updatedAt?: string;
 }
 
-export interface RequestWithoutId extends Omit<Request, '_id'> {}
+export interface RequestWithoutId extends Omit<Request, "_id"> {}
 
 export interface Reviewer {
-  reviewerid: string
-  reviewerName: string
-  reviewerEmail: string
-  role: string
-  feedbackSubmitted: boolean
+  reviewerid: string;
+  reviewerName: string;
+  reviewerEmail: string;
+  role: string;
+  feedbackSubmitted: boolean;
 }
 
 export interface Question {
-  _id: string
-  question: string
-  isFreeForm: boolean
+  _id: string;
+  question: string;
+  isFreeForm: boolean;
 }
 
 export interface Section {
-  _id: string
-  sectionName: string
-  questions: Question[]
+  _id: string;
+  sectionName: string;
+  questions: Question[];
 }
 
 export interface CustomFormData {
-  requestid: string
-  employeeid: string
+  requestid: string;
+  employeeid: string;
   answers: {
     [sectionId: string]: {
-      [questionId: string]: string | number
-    }
-  }
+      [questionId: string]: string | number;
+    };
+  };
 }
