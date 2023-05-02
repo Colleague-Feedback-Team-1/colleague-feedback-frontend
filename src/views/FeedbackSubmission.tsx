e987da2d999aa8036affe1e477f772ac368d4648
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-02 14:53:21 +0300

    Enum

--- src/views/FeedbackSubmission.tsx
+++ src/views/FeedbackSubmission.tsx
@@ -1,93 +1,85 @@
-import { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import FeedbackForm from "../components/FeedbackForm";
-import { Section, CustomFormData } from "../types/types";
-import Loading from "../components/Loading";
-import FormIntro from "../assets/FormIntro";
-import UserContext from "../context/UserContext";
-import { UserContextProps, Request, Reviewer } from "../types/types";
-import { Container, Box } from "@mui/material";
-import { useParams, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import FeedbackForm from '../components/FeedbackForm'
+import { Section, CustomFormData } from '../types/types'
+import Loading from '../components/Loading'
+import FormIntro from '../assets/FormIntro'
+import UserContext from '../context/UserContext'
+import { UserContextProps, Request, Reviewer } from '../types/types'
+import { Container, Box } from '@mui/material'
+import { useParams, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 type RouteParams = {
-  requestId: string;
-};
+  requestId: string
+}
 
 function FeedbackSubmission() {
-  const params = useParams<RouteParams>();
-  const [requestData, setRequestData] = useState<Request | null>();
-  const [data, setData] = useState<Section[]>([]);
-  const { user } = useContext<UserContextProps>(UserContext);
-  const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
+  const params = useParams<RouteParams>()
+  const [requestData, setRequestData] = useState<Request | null>()
+  const [data, setData] = useState<Section[]>([])
+  const { user } = useContext<UserContextProps>(UserContext)
+  const [isLoading, setIsLoading] = useState(true)
+  const navigate = useNavigate()
   const [userRoleOnRequest, setUserRoleOnRequest] = useState<
-    "reviewee" | "reviewer" | "manager" | null
-  >(null);
+    'reviewee' | 'reviewer' | 'manager' | null
+  >(null)
 
   // check role
   const checkRole = () => {
     if (user?._id === requestData?.employeeid) {
-      setUserRoleOnRequest("reviewee");
+      setUserRoleOnRequest('reviewee')
     } else if (user?._id === requestData?.assignedManagerid) {
-      setUserRoleOnRequest("manager");
+      setUserRoleOnRequest('manager')
     } else {
       requestData?.reviewers.map((reviewer) => {
-        if (
-          user?._id === reviewer.reviewerid &&
-          reviewer.feedbackSubmitted === false
-        ) {
-          setUserRoleOnRequest("reviewer");
+        if (user?._id === reviewer.reviewerid && reviewer.feedbackSubmitted === false) {
+          setUserRoleOnRequest('reviewer')
         }
-        return;
-      });
+        return
+      })
     }
-  };
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:4500/api/questions/"
-        );
-        setData(response.data);
-        setIsLoading(false);
+        const response = await axios.get('http://localhost:4500/api/questions/')
+        setData(response.data)
+        setIsLoading(false)
       } catch (error) {
-        console.error("Error fetching data:", error);
-        setIsLoading(false);
+        console.error('Error fetching data:', error)
+        setIsLoading(false)
       }
-    };
+    }
 
-    fetchData();
-    fetchRequestByRequestId(params.requestId!);
-    checkRole();
-  }, []);
+    fetchData()
+    fetchRequestByRequestId(params.requestId!)
+    checkRole()
+  }, [])
 
   const fetchRequestByRequestId = async (requestId: string) => {
     try {
       const response = await axios.get(
         `http://localhost:4500/api/review-requests/by-requestid/${requestId}`
-      );
-      setRequestData(response.data);
+      )
+      setRequestData(response.data)
     } catch (error) {
-      console.error("Error fetching request data:", error);
-      return null;
+      console.error('Error fetching request data:', error)
+      return null
     }
-  };
+  }
 
   const submitFeedback = async (requestData: any) => {
     try {
-      await axios.post(
-        "http://localhost:4500/api/feedback-data/insert-feedback",
-        requestData
-      );
-      console.log("Form submitted successfully");
-      navigate("/");
+      await axios.post('http://localhost:4500/api/feedback-data/insert-feedback', requestData)
+      console.log('Form submitted successfully')
+      navigate('/')
     } catch (error) {
-      console.error("Error submitting form:", error);
-      toast.error("Error submitting form!");
+      console.error('Error submitting form:', error)
+      toast.error('Error submitting form!')
     }
-  };
+  }
 
   const updateReviewerFeedbackStatus = async (
     requestId: string,
@@ -99,77 +91,60 @@ function FeedbackSubmission() {
         await axios.patch(
           `http://localhost:4500/api/review-requests/update-status/${requestId}/${reviewerObjectId}`,
           { feedbackSubmitted: true }
-        );
+        )
       } catch (error) {
-        console.error("Error updating reviewer feedback status:", error);
+        console.error('Error updating reviewer feedback status:', error)
       }
     } else {
-      console.log("Update self review status");
+      console.log('Update self review status')
       try {
-        await axios.patch(
-          `http://localhost:4500/api/review-requests/update-status/${requestId}`,
-          { selfReview: true }
-        );
+        await axios.patch(`http://localhost:4500/api/review-requests/update-status/${requestId}`, {
+          selfReview: true,
+        })
       } catch (error) {
-        console.error("Error updating reviewer feedback status:", error);
+        console.error('Error updating reviewer feedback status:', error)
       }
     }
-  };
+  }
 
   const handleSubmit = async (formData: CustomFormData) => {
-    const employeeId = requestData?.employeeid;
+    const employeeId = requestData?.employeeid
     const reviewerData = requestData!.reviewers.find(
       (reviewer: Reviewer) => reviewer.reviewerid === user?._id
-    );
+    )
 
-    const sections = Object.entries(formData.answers).map(
-      ([sectionId, sectionData]) => {
-        const sectionName = data.find(
-          (section) => section._id === sectionId
-        )?.sectionName;
-        const questions = Object.entries(sectionData).map(
-          ([questionId, answer]) => {
-            const question = data
-              .flatMap((section) => section.questions)
-              .find((question) => question._id === questionId);
+    const sections = Object.entries(formData.answers).map(([sectionId, sectionData]) => {
+      const sectionName = data.find((section) => section._id === sectionId)?.sectionName
+      const questions = Object.entries(sectionData).map(([questionId, answer]) => {
+        const question = data
+          .flatMap((section) => section.questions)
+          .find((question) => question._id === questionId)
 
-            return question?.isFreeForm
-              ? { openFeedback: answer }
-              : { score: Number(answer) };
-          }
-        );
+        return question?.isFreeForm ? { openFeedback: answer } : { score: Number(answer) }
+      })
 
-        return { sectionName, questions };
-      }
-    );
+      return { sectionName, submittedBy: userRoleOnRequest, questions }
+    })
 
     const submitData = {
       requestid: params.requestId,
       employeeid: employeeId,
       sections,
-    };
+    }
 
     try {
-      await submitFeedback(submitData);
-      console.log("Form submitted successfully");
+      await submitFeedback(submitData)
+      console.log('Form submitted successfully')
 
       if (reviewerData && reviewerData.reviewerid) {
-        await updateReviewerFeedbackStatus(
-          params.requestId!,
-          reviewerData.reviewerid,
-          false
-        );
+        await updateReviewerFeedbackStatus(params.requestId!, reviewerData.reviewerid, false)
       } else {
-        await updateReviewerFeedbackStatus(
-          params.requestId!,
-          requestData!._id,
-          true
-        );
+        await updateReviewerFeedbackStatus(params.requestId!, requestData!._id, true)
       }
     } catch (error) {
-      console.error("Error submitting form:", error);
+      console.error('Error submitting form:', error)
     }
-  };
+  }
 
   return (
     <>
@@ -186,7 +161,7 @@ function FeedbackSubmission() {
         </Box>
       </Container>
     </>
-  );
+  )
 }
 
-export default FeedbackSubmission;
+export default FeedbackSubmission
