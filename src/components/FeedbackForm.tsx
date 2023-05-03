c604798f4a10adfdec7c9186a6bb87735bb974b4
silvercoyote97 <haidang.levn@gmail.com>
2023-05-03 16:49:19 +0300

    Update styling for form

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -11,6 +11,7 @@ import {
   Grid,
   Button,
   Stack,
+  Box,
 } from "@mui/material";
 
 interface CustomFormProps {
@@ -28,55 +29,79 @@ const FeedbackForm: React.FC<CustomFormProps> = ({ data, onSubmit }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       {data.map((section) => (
-        <Grid key={section._id} container direction="column" spacing={2}>
-          <Grid item>
-            <Typography variant="h6">{section.sectionName}</Typography>
-          </Grid>
-          {section.questions.map((question) => (
-            <Grid item key={question._id}>
-              <Typography variant="body1">{question.question}</Typography>
-              {question.isFreeForm ? (
-                <Controller
-                  name={`answers.${section._id}.${question._id}`}
-                  rules={{ required: true }}
-                  control={control}
-                  render={({ field }) => (
-                    <TextField {...field} variant="outlined" fullWidth />
-                  )}
-                />
-              ) : (
-                <Controller
-                  name={`answers.${section._id}.${question._id}`}
-                  rules={{ required: true }}
-                  control={control}
-                  defaultValue={"3"}
-                  render={({ field }) => (
-                    <Stack
-                      direction={"row"}
-                      spacing={2}
-                      sx={{ alignItems: "center", justifyContent: "center" }}
-                    >
-                      <Typography>Very bad</Typography>
-                      <RadioGroup {...field} row>
-                        {[1, 2, 3, 4, 5].map((value) => (
-                          <FormControlLabel
-                            key={value}
-                            value={value.toString()} // React Hook Form requires string values
-                            control={<Radio />}
-                            label={value}
-                          />
-                        ))}
-                      </RadioGroup>
-                      <Typography>Very Good</Typography>
-                    </Stack>
-                  )}
-                />
-              )}
-            </Grid>
-          ))}
-        </Grid>
+        <Stack
+          key={section._id}
+          direction="row"
+          spacing={5}
+          sx={{
+            backgroundColor: "#ffdbeb",
+            border: "1px solid black",
+            marginBottom: "50px",
+            padding: "20px",
+          }}
+        >
+          <Stack
+            justifyContent={"center"}
+            textAlign={"left"}
+            maxWidth={"190px"}
+          >
+            <Typography variant="h4">
+              {section.sectionName.toUpperCase()}
+            </Typography>
+          </Stack>
+          <Stack direction={"column"} sx={{ width: "100%" }}>
+            {section.questions.map((question) => (
+              <Box key={question._id}>
+                <Typography variant="body1" fontWeight={"bold"}>
+                  {question.question}
+                </Typography>
+                {question.isFreeForm ? (
+                  <Controller
+                    name={`answers.${section._id}.${question._id}`}
+                    rules={{ required: true }}
+                    control={control}
+                    render={({ field }) => (
+                      <TextField {...field} variant="outlined" fullWidth />
+                    )}
+                  />
+                ) : (
+                  <Controller
+                    name={`answers.${section._id}.${question._id}`}
+                    rules={{ required: true }}
+                    control={control}
+                    defaultValue={"3"}
+                    render={({ field }) => (
+                      <Stack
+                        direction={"row"}
+                        spacing={2}
+                        sx={{ alignItems: "center", justifyContent: "center" }}
+                      >
+                        <Typography color={"red"} fontWeight={"bold"}>
+                          Very bad
+                        </Typography>
+                        <RadioGroup {...field} row>
+                          {[1, 2, 3, 4, 5].map((value) => (
+                            <FormControlLabel
+                              key={value}
+                              value={value.toString()} // React Hook Form requires string values
+                              control={<Radio />}
+                              label={value}
+                            />
+                          ))}
+                        </RadioGroup>
+                        <Typography color={"green"} fontWeight={"bold"}>
+                          Very Good
+                        </Typography>
+                      </Stack>
+                    )}
+                  />
+                )}
+              </Box>
+            ))}
+          </Stack>
+        </Stack>
       ))}
-      <Grid item>
+      <Box>
         <Button
           type="submit"
           variant="contained"
@@ -85,7 +110,7 @@ const FeedbackForm: React.FC<CustomFormProps> = ({ data, onSubmit }) => {
         >
           Submit Feedback
         </Button>
-      </Grid>
+      </Box>
     </form>
   );
 };
