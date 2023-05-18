45c8297420d9944fb2280d650b7c1abeb897e090
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-18 11:39:22 +0300

    Implemented Margits edits to FeedbackForm

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,5 +1,3 @@
-// imports
-import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { QuestionSection, CustomFormData } from "../types/types";
 import {
@@ -11,6 +9,7 @@ import {
   Button,
   Stack,
   Box,
+  Grid,
 } from "@mui/material";
 
 interface CustomFormProps {
@@ -28,82 +27,81 @@ const FeedbackForm: React.FC<CustomFormProps> = ({ data, onSubmit }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       {data.map((section) => (
-        <Stack
-          key={section._id}
-          direction="row"
-          spacing={5}
-          sx={{
-            backgroundColor: "#ffdbeb",
-            border: "1px solid black",
-            marginBottom: "50px",
-            padding: "20px",
-          }}
-        >
+        <Grid container direction="column" alignItems="center" key={section._id}>
+          <Typography variant="h6" sx={{ marginBottom: "0.6rem" }}>
+            {section.sectionName}
+          </Typography>
           <Stack
-            justifyContent={"center"}
-            textAlign={"left"}
-            maxWidth={"190px"}
+            direction="row"
+            spacing={5}
+            sx={{
+              marginBottom: "50px",
+              padding: "20px",
+              width: "90%",
+              backgroundColor: "hsl(0deg 5.71% 86.27% / 14.9%)",
+              boxShadow:"0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+              borderRadius:"4px"
+            }}
           >
-            <Typography variant="h4">
-              {section.sectionName.toUpperCase()}
-            </Typography>
+            <Stack direction={"column"} sx={{ width: "100%" }}>
+              {section.questions.map((question) => (
+                <Box key={question._id} marginBottom={2}>
+                  <Typography
+                    variant="body1"
+                    padding={"10px 0"}
+                  >
+                    {question.question}:
+                  </Typography>
+                  {question.isFreeForm ? (
+                    <Controller
+                      name={`answers.${section._id}.${question._id}`}
+                      rules={{ required: true }}
+                      control={control}
+                      render={({ field }) => (
+                        <TextField {...field} variant="outlined" fullWidth />
+                      )}
+                    />
+                  ) : (
+                    <Controller
+                      name={`answers.${section._id}.${question._id}`}
+                      rules={{ required: true }}
+                      control={control}
+                      defaultValue={"3"}
+                      render={({ field }) => (
+                        <Stack
+                          direction={"row"}
+                          spacing={2}
+                          sx={{ 
+                            alignItems: "center", 
+                            justifyContent: "center",
+                            margin: '0.8rem 0'
+                          }}
+                        >
+                          <Typography color={"red"}>
+                            Strongly <br /> Disagree
+                          </Typography>
+                          <RadioGroup {...field} row>
+                            {[1, 2, 3, 4, 5].map((value) => (
+                              <FormControlLabel
+                                key={value}
+                                value={value.toString()} // React Hook Form requires string values
+                                control={<Radio />}
+                                label={value}
+                              />
+                            ))}
+                          </RadioGroup>
+                          <Typography color={"green"}>
+                            Strongly <br /> Agree
+                          </Typography>
+                        </Stack>
+                      )}
+                    />
+                  )}
+                </Box>
+              ))}
+            </Stack>
           </Stack>
-          <Stack direction={"column"} sx={{ width: "100%" }}>
-            {section.questions.map((question) => (
-              <Box key={question._id}>
-                <Typography
-                  variant="body1"
-                  fontWeight={"bold"}
-                  padding={"10px 0"}
-                  textAlign={"justify"}
-                >
-                  {question.question}:
-                </Typography>
-                {question.isFreeForm ? (
-                  <Controller
-                    name={`answers.${section._id}.${question._id}`}
-                    rules={{ required: true }}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField {...field} variant="outlined" fullWidth />
-                    )}
-                  />
-                ) : (
-                  <Controller
-                    name={`answers.${section._id}.${question._id}`}
-                    rules={{ required: true }}
-                    control={control}
-                    defaultValue={"3"}
-                    render={({ field }) => (
-                      <Stack
-                        direction={"row"}
-                        spacing={2}
-                        sx={{ alignItems: "center", justifyContent: "center" }}
-                      >
-                        <Typography color={"red"} fontWeight={"bold"}>
-                          Strongly <br /> Disagree
-                        </Typography>
-                        <RadioGroup {...field} row>
-                          {[1, 2, 3, 4, 5].map((value) => (
-                            <FormControlLabel
-                              key={value}
-                              value={value.toString()} // React Hook Form requires string values
-                              control={<Radio />}
-                              label={value}
-                            />
-                          ))}
-                        </RadioGroup>
-                        <Typography color={"green"} fontWeight={"bold"}>
-                          Strongly <br /> Agree
-                        </Typography>
-                      </Stack>
-                    )}
-                  />
-                )}
-              </Box>
-            ))}
-          </Stack>
-        </Stack>
+        </Grid>
       ))}
       <Box>
         <Button
